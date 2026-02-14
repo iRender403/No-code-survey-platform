6382a63156fb5578f9c65b77d0b9950757b0efaa
iRender403 <tianxiawuzei1234>
2026-02-14 22:02:08 +0800

    成功加入表单，初步定义表单数据类型

--- no-code-survey-platform/src/App.tsx
+++ no-code-survey-platform/src/App.tsx
@@ -1,36 +1,80 @@
-import {
-  ConfigProvider,
-  Button,
-  Flex
-} from "antd"
+//===================================
+//  配置引入
+//===================================
+
+// 引入UI组件
+import { Button, Flex, Table } from "antd"
 // 引入图标
-import {
-  PlusOutlined,
-  CompassFilled,
+import { PlusOutlined, CompassFilled } from '@ant-design/icons';
+// 引入
+//===================================
+//   定义接口类型
+//===================================
 
-} from '@ant-design/icons';
+// 定义数据接口
+interface DataType{
+  id:string,
+  date:string,
+  title:string,
+  count:number,
+  updateTime:string,
+  operator:string,
+}
 
+//=======================================
+//   数据
+//=======================================
+const columns: any[] = [
+  {
+    title: '创建日期',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: '问卷标题',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: '题目数',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: '最近更新日期',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: '操作',
+    dataIndex: 'name',
+    key: 'name'
+  },
+]
 
+const tableData: Array<DataType> = [];
+
+//===================================
+//   ....
+//===================================
 function App() {
   return (
     <>
       <div className="container">
         <h1 className="font-weight-100 text-center">低代码平台 - 启动</h1>
         {/* 按钮组 */}
-        <ConfigProvider >
-          <Flex gap="small">
-            <Button color="blue" variant="solid" icon={<PlusOutlined />}>创建问卷</Button>
-            <Button color="green" variant="solid" icon={<CompassFilled />}>组件市场</Button>
-          </Flex>
-        </ConfigProvider>
-
+        <Flex gap="small">
+          <Button color="blue" variant="solid" icon={<PlusOutlined />}>创建问卷</Button>
+          <Button color="green" variant="solid" icon={<CompassFilled />}>组件市场</Button>
+        </Flex>
         {/* 数据表格 */}
-        <ConfigProvider>
-            <Flex>
-              
-            </Flex>
-        </ConfigProvider>
-
+        <Flex>
+          <Table
+            dataSource={tableData}
+            columns={columns}
+          >
+          </Table>
+        </Flex>
       </div>
     </>
   )
--- 里程碑/bug记录.md
+++ 里程碑/bug记录.md
@@ -1,1 +1,3 @@
 1. antDesign中的 color和type:primary 之间的冲突
+
+2. 明天的任务：第一个完成表格部分
