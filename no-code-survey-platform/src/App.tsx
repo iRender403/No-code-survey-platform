7b3e76c324de003d9cd2654b7ad17828849c85e4
iRender403 <tianxiawuzei1234>
2026-02-14 09:04:47 +0800

    创建vite+react项目

---
+++ no-code-survey-platform/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+function App() {
+
+
+  return (
+    <>
+      <div style={{ padding: '20px' }}>
+        <h1>低代码平台 - 启动</h1>
+        <p>画布准备就绪...</p>
+      </div>
+    </>
+  )
+}
+
+export default App
