fbf330887af14793b53708999ecef10dc96536f2
iRender403 <tianxiawuzei1234>
2026-02-16 22:44:18 +0800

    第一部分完成与第一次复盘

--- no-code-survey-platform/src/App.tsx
+++ no-code-survey-platform/src/App.tsx
@@ -1,80 +1,130 @@
-//===================================
+//================================================
 //  配置引入
-//===================================
-
+//================================================
 // 引入UI组件
-import { Button, Flex, Table } from "antd"
+import { Button, Space, Table, ConfigProvider } from "antd"
 // 引入图标
 import { PlusOutlined, CompassFilled } from '@ant-design/icons';
-// 引入
-//===================================
+// 引入 数据类型
+
+//================================================
 //   定义接口类型
-//===================================
+//================================================
 
-// 定义数据接口
-interface DataType{
-  id:string,
-  date:string,
-  title:string,
-  count:number,
-  updateTime:string,
-  operator:string,
-}
 
-//=======================================
+//#region  数据
+//================================================
 //   数据
-//=======================================
-const columns: any[] = [
+//================================================
+const columns = [
   {
-    title: '创建日期',
-    dataIndex: 'name',
-    key: 'name'
+    title: "创建日期",
+    dataIndex: "createAt",        //对应数据里面的字段名
+    key: "createAt",
+    width:150,
+    fixed:"start",
+    
   },
   {
-    title: '问卷标题',
-    dataIndex: 'name',
-    key: 'name'
+    title: "问卷标题",
+    dataIndex: "title",           //对应数据里面的字段名
+    key: "title",
+    
+
   },
   {
-    title: '题目数',
-    dataIndex: 'name',
-    key: 'name'
+    title: "题目数",
+    dataIndex: "count",           //对应数据里面的字段名
+    key: "count",
+    align:"center",
+    width:150,
   },
   {
-    title: '最近更新日期',
-    dataIndex: 'name',
-    key: 'name'
+    title: "最新更新日期",
+    dataIndex: "update",        //对应数据里面的字段名
+    key: "update",
+    align:"center",
+    width:150,
   },
   {
-    title: '操作',
-    dataIndex: 'name',
-    key: 'name'
+    title: "操作",
+    dataIndex: "operator",        //对应数据里面的字段名
+    key: "operator",
+    align:"center",
+    width:300,
+    fixed:"end",
+    //
+    render: (_, record) => (
+      <Space>
+        <a>查看问卷</a>
+        <a>编辑</a>
+        <a>删除</a>
+      </Space>
+    ),
   },
 ]
 
-const tableData: Array<DataType> = [];
-
+// 模拟数据
+const dataSource = [
+  {
+    key: '1',
+    createAt: '2026/02/16 10:07:39',
+    title: '关于 AI 学习工具的调研',
+    count: 10,
+    update: '2026-02-16',
+  },
+]
+//#endregion
 //===================================
 //   ....
 //===================================
 function App() {
   return (
     <>
-      <div className="container">
+      {/* 容器 */}
+      <div style={{ padding: "20px" }}>
+        {/* Header */}
         <h1 className="font-weight-100 text-center">低代码平台 - 启动</h1>
-        {/* 按钮组 */}
-        <Flex gap="small">
-          <Button color="blue" variant="solid" icon={<PlusOutlined />}>创建问卷</Button>
-          <Button color="green" variant="solid" icon={<CompassFilled />}>组件市场</Button>
-        </Flex>
-        {/* 数据表格 */}
-        <Flex>
-          <Table
-            dataSource={tableData}
-            columns={columns}
+        {/* Body */}
+        <div>
+          {/* 按钮组 */}
+          <Space >
+            <Button type='primary' icon={<PlusOutlined />}>创建问卷</Button>
+            <Button type='primary'
+              icon={<CompassFilled />}
+              style={{ backgroundColor: '#52c41a', borderColor: '#52c41a' }}
+            >组件市场</Button>
+          </Space>
+          <ConfigProvider
+            theme={{
+              token: {
+                borderRadius: 0,
+              },
+              components: {
+                Table: {
+                  lineHeight:1,
+                  headerColor: 'rgba(0, 0, 0, 0.45)',
+                  headerBg: "#fff",
+                }
+              }
+            }}
           >
-          </Table>
-        </Flex>
+            {/* 数据表格 */}
+            <Table
+              scroll={{ x: 'max-content' }}
+              columns={columns}
+              dataSource={dataSource}
+              bordered               // 创建边框
+              style={{
+                marginTop: "15px",
+              }}
+              pagination={false}     // 不创建多页
+            ></Table>
+          </ConfigProvider>
+
+
+        </div>
+
       </div>
     </>
   )
--- 里程碑/bug记录.md
+++ 里程碑/bug记录.md
@@ -1,3 +1,19 @@
 1. antDesign中的 color和type:primary 之间的冲突
+type的优先级最高，如果要修改，最好还是引入模式，solid是实体按钮
 
 2. 明天的任务：第一个完成表格部分
+
+3. TableColumnsType<DataType>
+对象字面量只能指定已知属性，并且“id”不在类型“ColumnGroupType<DataType>
+
+4. 为什么不能使用scss在app之中
+
+5. 数据是怎样加进去的
+
+6. 我的表格还需要微调，有什么方案
+
+7. render 为什么不需要手动传数据
+
+8. css属性最高权重的问题：不要修补表象，去F12溯源那个决定最终像素的css变量或属性，并用最高权重的配置来覆盖他
+
+9. render: (_, record) 参数类型问题
